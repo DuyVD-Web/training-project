5957a755245fb7f43e0f91d8cec0ddac80116d31
Duy <vuducduy1012@gmail.com>
2024-11-15 10:58:44 +0700

    Add login register UI

---
+++ app/Http/Controllers/SessionController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Validation\ValidationException;
+
+class SessionController extends Controller
+{
+    public function create()
+    {
+        return view('auth.login');
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,18 @@
 <?php
 
+use App\Http\Controllers\RegisterUserController;
+use App\Http\Controllers\SessionController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/login', function () {
+    return view('auth.login');
+});
+
+
+Route::get('/register',[RegisterUserController::class,'index']);
+
+Route::post('/login', [SessionController::class, 'store']);
